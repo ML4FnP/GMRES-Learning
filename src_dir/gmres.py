10fd4c8a515f2940e7a1065c499245078b472c3c
Johannes Blaschke <jpblaschke@lbl.gov>
2020-05-08 15:13:41 -0700

    implement tolerance in GMRES

--- src_dir/gmres.py
+++ src_dir/gmres.py
@@ -10,13 +10,25 @@ import numpy as np
 
 
 def GMRES(A, b, x0, e, nmax_iter, restart=None):
+    """
+    Quick and dirty GMRES -- TODO: optimize mem footprint when going to larger
+    systems.
+    """
+
+    # TODOs for this function:
+    # 1. list -> numpy.array <= better memory access
+    # 2. don't append to lists -> prealoc and slice
+    # 3. add documentation -- this will probably never happen :P
+    
+    normb = np.linalg.norm(b)
+    if normb == 0.0:
+        normb = 1.0
+
+    #TODO: use matmul_a for consistency here?
     r = b - np.asarray(np.dot(A, x0)).reshape(-1)
-
-    x = []
-    q = [0] * (nmax_iter)
-
-    x.append(r)
-
+    
+    x    = [r]
+    q    = [0] * (nmax_iter)
     q[0] = r / np.linalg.norm(r)
 
     h = np.zeros((nmax_iter + 1, nmax_iter))
@@ -31,13 +43,20 @@ def GMRES(A, b, x0, e, nmax_iter, restart=None):
         if (h[k + 1, k] != 0 and k != nmax_iter - 1):
             q[k + 1] = y / h[k + 1, k]
 
-        b = np.zeros(nmax_iter + 1)
-        b[0] = np.linalg.norm(r)
+        b_inner    = np.zeros(nmax_iter + 1)
+        b_inner[0] = np.linalg.norm(r)
 
-        result = np.linalg.lstsq(h, b, rcond=None)[0]
+        result  = np.linalg.lstsq(h, b_inner, rcond=None)[0]
+        g       = np.dot(np.asarray(q).transpose(), result)
+        r_inner = b - (g + x0)
 
-        x.append(np.dot(np.asarray(q).transpose(), result) + x0)
+        x.append(g + x0)
 
+        # break out if the residual is lower than threshold
+        if np.linalg.norm(r_inner)/normb < e*A.shape[0]:
+            break
+
+ 
     return x
 
 
